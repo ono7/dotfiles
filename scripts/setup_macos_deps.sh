46d0d9eef4957e124663bb242da00e7b73ab183c
jlima <jlima@mbp>
2024-08-04 20:03:24 -0500

    updates to setup_macos_deps.sh

--- scripts/setup_macos_deps.sh
+++ scripts/setup_macos_deps.sh
@@ -2,15 +2,14 @@
 
 echo "$0"
 
-echo '========== install homebrew =========='
-
-[ ! -d /opt/homebrew/bin ] && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-
-echo '========== install brew packages =========='
-
-brew install ansifilter wget tree go neovim rar clang-format zoxide grep netcat
-brew install fd cmake ack rg coreutils ssh-copy-id jq p7zip curl tmux universal-ctags mtr lua ninja rust npm starship stow
-brew install bpytop pinentry-mac npm
-brew install golang delve
-brew install --cask alacritty
-npm install lua-fmt prettier jsonlint typescript eslint jsonlint doctoc -g
+if command -v homebrew &> /dev/null; then
+  brew install ansifilter wget tree go neovim rar clang-format zoxide grep netcat
+  brew install fd cmake ack rg coreutils ssh-copy-id jq p7zip curl tmux universal-ctags mtr lua ninja rust npm starship stow
+  brew install bpytop pinentry-mac npm
+  brew install golang delve
+  brew install --cask alacritty
+  npm install lua-fmt prettier jsonlint typescript eslint jsonlint doctoc -g
+else
+  echo "Install homebrew first!"
+  exit 1
+fi
