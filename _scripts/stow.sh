65989891bcfba1d5274df157039ff78c6b4068ec
jlima <jlima@mbp>
2024-08-04 22:34:15 -0500

    fix stow

--- _scripts/stow.sh
+++ _scripts/stow.sh
@@ -9,17 +9,17 @@ echo "Running stow..."
 export PATH="/opt/homebrew/sbin:/usr/local/sbin:$PATH"
 
 if command -v stow &> /dev/null; then
-  stow -R alacritty
-  stow -R bash
-  stow -R ctags
-  stow -R gdb
-  stow -R git
-  stow -R nvim
-  stow -R ssh
-  stow -R sqlite
-  stow -R starship
-  stow -R tmux
-  stow -R zsh
+  stow alacritty
+  stow bash
+  stow ctags
+  stow gdb
+  stow git
+  stow nvim
+  stow ssh
+  stow sqlite
+  stow starship
+  stow tmux
+  stow zsh
 else
     echo "stow is not installed, please install stow first..."
     exit 1
