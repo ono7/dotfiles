8d54d47845c14ecc61a8114f0b24f52d040056f1
jlima <jlima@mbp>
2024-08-05 16:52:50 -0500

    updates to setup_linux_deps.sh

--- _scripts/setup_linux_deps.sh
+++ _scripts/setup_linux_deps.sh
@@ -24,10 +24,11 @@ rm -rf ~/nvim
 rm -rf ~/nvim-linux64
 rm -f ~/local/bin/nvim
 rm -f ~/local/bin/shortpath
+rm -rf ~/nvim-linux64.*
 wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
 tar xzf nvim-linux64.tar.gz
 mv nvim-linux64 nvim
-rm nvim-linux64.tar.gz
+rm nvim-linux64.*
 ln -sf ~/nvim/bin/nvim ~/local/bin/nvim
 echo "Neovim setup for linux complete"
 
