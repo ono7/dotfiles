461c35b9bd7b311569be637974bae667f8d6cb25
jlima <jlima@mbp>
2024-08-07 19:41:32 -0500

    updates to gruvbox.lua

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -39,25 +39,14 @@ my_gitsigns.setup({
       return "<Ignore>"
     end, { expr = true })
 
-    -- Actions
-    -- map({ "n", "v" }, "<leader>hs", ":Gitsigns stage_hunk<CR>")
-    -- map({ "n", "v" }, "<leader>hr", ":Gitsigns reset_hunk<CR>")
-    -- map("n", "<leader>ss", gs.stage_buffer)
-    -- map("n", "<leader>su", gs.undo_stage_hunk)
-    -- map("n", "<leader>sR", gs.reset_buffer)
     map("n", "<leader>sp", gs.preview_hunk)
     map("n", "<leader>sb", function()
       gs.blame_line({ full = true })
     end)
-    -- map("n", "<leader>tb", gs.toggle_current_line_blame)
     map("n", "<leader>sd", gs.diffthis)
     map("n", "<leader>sD", function()
       gs.diffthis("~")
     end)
-    -- map("n", "<leader>td", gs.toggle_deleted)
-
-    -- Text object
-    -- map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
   end,
   signs = {
     add = { text = "│" },
