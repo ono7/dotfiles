0cd70ddf3f015a49d4edba607d9fedc8ca6f3c49
jlima <jlima@mbp>
2024-08-25 15:52:48 -0500

    M  nvim/.config/nvim/init.lua M  nvim/.config/nvim/lua/config/cmds.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -37,8 +37,8 @@ vim.g.netrw_banner = 0
 vim.g.netrw_browse_split = 0
 vim.g.netrw_liststyle = 3
 
-vim.opt.winbar = "%=%-.75F %-m {%{get(b:, 'branch_name', '')}}"
--- vim.opt.winbar = "%=%-.75F %-m %{FugitiveStatusline()}"
+-- vim.opt.winbar = "%=%-.75F %-m {%{get(b:, 'branch_name', '')}}"
+vim.opt.winbar = "%=%-.75F %-m %{FugitiveStatusline()}"
 -- require("config.winbar")
 
 require("config.disabled")
