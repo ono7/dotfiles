f9d8168ce89007a4982b421921ed8723ec74df2e
jlima <jlima@mbp>
2024-09-03 02:04:03 -0500

    M  nvim/.config/nvim/lua/config/lazy-config.lua

--- nvim/.config/nvim/lua/config/lazy-config.lua
+++ nvim/.config/nvim/lua/config/lazy-config.lua
@@ -119,7 +119,12 @@ require("lazy").setup({
   {
     "monkoose/matchparen.nvim",
     config = function()
-      require('matchparen').setup()
+      require('matchparen').setup({
+        on_startup = true,           -- Should it be enabled by default
+        hl_group = 'MatchParen',     -- highlight group of the matched brackets
+        augroup_name = 'matchparen', -- almost no reason to touch this unless there is already augroup with such name
+        debounce_time = 25,          -- debounce time in milliseconds for rehighlighting of brackets.
+      })
     end
   },
   {
