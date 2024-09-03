917aa2711a89ed2f6bfc9a2dc11957503db7ab7a
jlima <jlima@mbp>
2024-09-03 02:16:39 -0500

    M  nvim/.config/nvim/lua/config/lazy-config.lua; increase debounce time for matchparen lua Plugin

--- nvim/.config/nvim/lua/config/lazy-config.lua
+++ nvim/.config/nvim/lua/config/lazy-config.lua
@@ -123,7 +123,7 @@ require("lazy").setup({
         on_startup = true,           -- Should it be enabled by default
         hl_group = 'MatchParen',     -- highlight group of the matched brackets
         augroup_name = 'matchparen', -- almost no reason to touch this unless there is already augroup with such name
-        debounce_time = 25,          -- debounce time in milliseconds for rehighlighting of brackets.
+        debounce_time = 40,          -- debounce time in milliseconds for rehighlighting of brackets.
       })
     end
   },
