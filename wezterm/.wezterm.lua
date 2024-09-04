1d1d258a966df7979ffa17f11ce58f0080d6b79e
jlima <jlima@mbp>
2024-09-03 22:48:44 -0500

    chore(updates): wezterm.lua returns object instead of a table of config options

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,7 +1,8 @@
-local wezterm = require("wezterm")
-local scheme = wezterm.get_builtin_color_schemes()['Catppuccin Mocha']
+local wezterm                                     = require("wezterm")
+local scheme                                      = wezterm.get_builtin_color_schemes()['Catppuccin Mocha']
+local config                                      = wezterm.config_builder()
 
-local ansi = {
+local ansi                                        = {
   "#45475a",
   "#f38ba8",
   "#ceeac8",
@@ -17,92 +18,91 @@ local ansi = {
 }
 
 -- color overrides
-scheme.ansi = ansi
-scheme.brights = ansi
+scheme.ansi                                       = ansi
+scheme.brights                                    = ansi
 
 -- scheme.background = "#161616"
 -- scheme.cursor_bg = "#00acc1"
-scheme.cursor_bg = "#bac2de"
-scheme.cursor_fg = "#000"
-local act = wezterm.action
-
--- local colors, metadata = wezterm.color.load_scheme("~/.config/wezterm/theme_ayu.toml")
-return {
-  default_prog                               = { "/bin/zsh", "--login" },
-  audible_bell                               = "Disabled",
-  use_dead_keys                              = false,
-  scrollback_lines                           = 5000,
-  scroll_to_bottom_on_input                  = true,
-  default_cwd                                = wezterm.homedir,
-  disable_default_key_bindings               = true,
-  keys                                       = {
-    -- use xxd -psd to get hex char sequences
-    -- CTRL-SHIFT-l activates the debug overlay
-    { key = '0', mods = 'CTRL', action = act.ResetFontSize },
-    { key = '=', mods = 'CTRL', action = act.IncreaseFontSize },
-    { key = '-', mods = 'CTRL', action = act.DecreaseFontSize },
-    {
-      key = "w",
-      mods = "SHIFT|CTRL",
-      action = act.CloseCurrentPane({ confirm = false }),
-    },
-    {
-      -- turn off cmd+m to hide window from the os
-      key = "q",
-      mods = "CTRL",
-      action = act.DisableDefaultAssignment,
-    },
-    {
-      key = "v",
-      mods = "CTRL",
-      action = act.PasteFrom("Clipboard"),
-    },
-    {
-      -- delete word
-      key = "Backspace",
-      mods = "CTRL",
-      action = act.SendString("\x17"),
-    },
-    {
-      key = "Backspace",
-      mods = "ALT",
-      action = act.SendString("\x17"),
-    },
-    { key = 't', mods = 'CTRL|SHIFT', action = act.SpawnTab("DefaultDomain") },
-    { key = 'h', mods = 'CTRL|SHIFT', action = act.ActivateTabRelative(1) },
-    { key = 'l', mods = 'CTRL|SHIFT', action = act.ActivateTabRelative(-1) },
-  },
-  color_schemes                              = {
-    -- new or override scheme
-    ["Catppuccin Mocha"] = scheme,
-  },
-  color_scheme                               = 'Catppuccin Mocha',
-  font                                       = wezterm.font('MonoLisaNoLiga Nerd Font', { weight = "Medium" }),
-  adjust_window_size_when_changing_font_size = false,
-  window_close_confirmation                  = "NeverPrompt",
-  cursor_blink_rate                          = 0,
-  default_cursor_style                       = "SteadyBlock",
-  font_size                                  = 22,
-  line_height                                = 1.3,
-  initial_rows                               = 40,
-  initial_cols                               = 100,
-  underline_position                         = -8,
-  underline_thickness                        = 5,
-  window_background_opacity                  = 0.94,
-  macos_window_background_blur               = 20,
-
-  -- window_decorations                         = "RESIZE|MACOS_FORCE_ENABLE_SHADOW|MACOS_NS_VISUAL_EFFECT_MATERIAL_BLUR", -- will blurr eventually
-  window_decorations                         = "RESIZE",
-  hide_tab_bar_if_only_one_tab               = true,
-  front_end                                  = "WebGpu",
-  use_fancy_tab_bar                          = false,
-  enable_wayland                             = false,
-  -- front_end                                  = "Software",
-  webgpu_power_preference                    = "HighPerformance",
-  window_padding                             = {
-    left = 20,
-    right = 20,
-    top = 5,
-    bottom = 5,
-  },
+scheme.cursor_bg                                  = "#bac2de"
+scheme.cursor_fg                                  = "#000"
+local act                                         = wezterm.action
+config.default_prog                               = { "/bin/zsh", "--login" }
+config.audible_bell                               = "Disabled"
+config.use_dead_keys                              = false
+config.scrollback_lines                           = 5000
+config.scroll_to_bottom_on_input                  = true
+config.default_cwd                                = wezterm.homedir
+config.disable_default_key_bindings               = true
+config.color_schemes                              = {
+  -- new or override scheme
+  ["Catppuccin Mocha"] = scheme,
 }
+config.color_scheme                               = 'Catppuccin Mocha'
+
+config.font                                       = wezterm.font('MonoLisaNoLiga Nerd Font', { weight = "Medium" })
+config.adjust_window_size_when_changing_font_size = false
+config.window_close_confirmation                  = "NeverPrompt"
+config.cursor_blink_rate                          = 0
+config.default_cursor_style                       = "SteadyBlock"
+config.font_size                                  = 22
+config.line_height                                = 1.3
+config.initial_rows                               = 40
+config.initial_cols                               = 100
+config.underline_position                         = -8
+config.underline_thickness                        = 5
+config.window_background_opacity                  = 0.94
+config.macos_window_background_blur               = 20
+-- window_decorations                         = "RESIZE|MACOS_FORCE_ENABLE_SHADOW|MACOS_NS_VISUAL_EFFECT_MATERIAL_BLUR", -- will blurr eventually
+config.window_decorations                         = "RESIZE"
+config.hide_tab_bar_if_only_one_tab               = true
+config.front_end                                  = "WebGpu"
+config.use_fancy_tab_bar                          = false
+config.enable_wayland                             = false
+-- front_end                                  = "Software"
+config.webgpu_power_preference                    = "HighPerformance"
+config.window_padding                             = {
+  left = 20,
+  right = 20,
+  top = 5,
+  bottom = 5,
+}
+
+config.keys                                       = {
+  -- use xxd -psd to get hex char sequences
+  -- CTRL-SHIFT-l activates the debug overlay
+  { key = '0', mods = 'CTRL', action = act.ResetFontSize },
+  { key = '=', mods = 'CTRL', action = act.IncreaseFontSize },
+  { key = '-', mods = 'CTRL', action = act.DecreaseFontSize },
+  {
+    key = "w",
+    mods = "SHIFT|CTRL",
+    action = act.CloseCurrentPane({ confirm = false }),
+  },
+  {
+    -- turn off cmd+m to hide window from the os
+    key = "q",
+    mods = "CTRL",
+    action = act.DisableDefaultAssignment,
+  },
+  {
+    key = "v",
+    mods = "CTRL",
+    action = act.PasteFrom("Clipboard"),
+  },
+  {
+    -- delete word
+    key = "Backspace",
+    mods = "CTRL",
+    action = act.SendString("\x17"),
+  },
+  {
+    key = "Backspace",
+    mods = "ALT",
+    action = act.SendString("\x17"),
+  },
+  { key = 't', mods = 'CTRL|SHIFT', action = act.SpawnTab("DefaultDomain") },
+  { key = 'h', mods = 'CTRL|SHIFT', action = act.ActivateTabRelative(1) },
+  { key = 'l', mods = 'CTRL|SHIFT', action = act.ActivateTabRelative(-1) },
+}
+
+return config
