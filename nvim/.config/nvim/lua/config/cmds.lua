9dca3d329d7c9aca571406132b3b4d4e75fdbeb2
Lima, Jose <20385388+ono7@users.noreply.github.com>
2024-09-12 08:30:12 -0500

    starts commit messages in insert mode

--- nvim/.config/nvim/lua/config/cmds.lua
+++ nvim/.config/nvim/lua/config/cmds.lua
@@ -20,6 +20,7 @@ vim.api.nvim_create_autocmd({ "BufEnter" }, {
     vim.opt_local.spell = true
     vim.opt_local.wrap = false
     vim.api.nvim_win_set_cursor(0, { 1, 0 })
+    vim.cmd('startinsert')
   end,
 })
 
