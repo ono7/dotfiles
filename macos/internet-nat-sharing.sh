b1ebe5727c5d2cf2893e59f57d01a2b47d72301e
Lima, Jose <20385388+ono7@users.noreply.github.com>
2024-09-17 21:57:19 -0400

    updates to new nat script for sharing internet

--- macos/internet-nat-sharing.sh
+++ macos/internet-nat-sharing.sh
@@ -3,13 +3,15 @@
 # mikrotik(dhcp server) -> mac usbEth(dhcp client)
 # set default route on mikrotik to point to Mac dhcp client IP
 #
-sysctl -w net.inet.ip.forwarding=1
-# sysctl -w net.inet6.ip6.forwarding=1
-pfctl -d
-pfctl -F all
-pfctl -f /Users/jlima/.dotfiles/_code/macos/internet-sharing/nat-rules.txt -e
+sudo sysctl -w net.inet.ip.forwarding=1
+# sudo sysctl -w net.inet6.ip6.forwarding=1
+sudo pfctl -d
+sudo pfctl -F all
+sudo pfctl -f /Users/jlima/.dotfiles/_code/macos/internet-sharing/nat-rules.txt -e
 
 # check status
 # sudo pfctl -s all
 # sudo pfctl -s nat
 # sudo pfctl -s rules
+
+echo "Please check the utun interface used by vpn"
