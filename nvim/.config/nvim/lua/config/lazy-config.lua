7c9746a715bab14d358758bd9ea19dd75b9b5a1c
Lima, Jose <20385388+ono7@users.noreply.github.com>
2024-09-23 11:04:11 -0500

    updates

--- nvim/.config/nvim/lua/config/lazy-config.lua
+++ nvim/.config/nvim/lua/config/lazy-config.lua
@@ -32,7 +32,16 @@ require("lazy").setup({
       local opts = { noremap = true, silent = false }
 
       -- Create a new note after asking for its title.
-      vim.api.nvim_set_keymap("n", "zn", "<Cmd>ZkNew { title = vim.fn.input('Title: ') }<CR>", opts)
+      -- vim.api.nvim_set_keymap("n", "zn", "<Cmd>ZkNew { title = vim.fn.input('Title: ') }<CR>", opts)
+
+      vim.keymap.set("n", "zn", function()
+        local title = vim.fn.input('Title: ')
+        if #title < 4 then
+          print("Title should be 4+ chars")
+          return
+        end
+        vim.cmd('ZkNew { title = "' .. title .. '" }')
+      end, opts)
 
       -- Open notes.
       vim.api.nvim_set_keymap("n", "zo", "<Cmd>ZkNotes { sort = { 'modified' } }<CR>", opts)
