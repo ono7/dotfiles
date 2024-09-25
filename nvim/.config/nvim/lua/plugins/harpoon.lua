80dc98460682936ddfa4a0c78fd0a06f76b6a603
Lima, Jose <20385388+ono7@users.noreply.github.com>
2024-09-24 20:48:24 -0500

    enable harpoon with Alt+m, (neio)

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -5,12 +5,12 @@ harpoon:setup()
 -- REQUIRED
 
 vim.keymap.set("n", "mm", function() harpoon:list():add() end)
-vim.keymap.set("n", "<D-m>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+vim.keymap.set("n", "<M-m>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-vim.keymap.set("n", "<D-n>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<D-e>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<D-i>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<D-o>", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<M-n>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<M-e>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<M-i>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<M-o>", function() harpoon:list():select(4) end)
 
 -- Toggle previous & next buffers stored within Harpoon list
 -- vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
