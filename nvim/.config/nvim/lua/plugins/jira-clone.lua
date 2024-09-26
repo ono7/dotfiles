47f5c00ff6fc02836613d25991702d53476321dc
Lima, Jose <20385388+ono7@users.noreply.github.com>
2024-09-26 00:08:24 -0500

    update s

--- nvim/.config/nvim/lua/plugins/jira-clone.lua
+++ nvim/.config/nvim/lua/plugins/jira-clone.lua
@@ -1,3 +1,5 @@
+local M = {}
+
 local curl = require('plenary.curl')
 local jira_main = require('plugins.jira-base')
 
@@ -5,14 +7,11 @@ local function clone_issue(issue_key, num_clones)
   if not jira_main.setup() then
     return
   end
-
   -- Encode credentials
   local auth = vim.base64.encode(jira_main.config.jira_email .. ':' .. jira_main.config.jira_api_token)
-
   for i = 1, num_clones do
     local clone_payload = {
       summary = string.format("CLONE - %s, %d", issue_key, i),
-      -- optionalFields = {},
       includeAttachments = false
     }
     -- this endpoint only valid for cloud base deployments of jira
@@ -23,7 +22,6 @@ local function clone_issue(issue_key, num_clones)
         ['Content-Type'] = 'application/json',
       },
     })
-
     if clone_response.status ~= 200 then
       P(clone_response)
     end
@@ -36,31 +34,31 @@ local function jira_clone_command(args)
     jira_main.display_message("Usage: JiraClone <issue-key> <number-of-clones>", vim.log.levels.ERROR)
     return
   end
-
   local issue_key = ""
-
   local preissue = args[1]
-  if preissue:match("^[Nn][Tt][Ww][Kw]-") then
+  if preissue:match("^[Nn][Tt][Ww][Kk]-") then
     issue_key = preissue
   elseif preissue:match("^%d+") then
     issue_key = string.format("NTWK-%d", preissue)
   end
-
   local num_clones = tonumber(args[2])
-
   if not num_clones or num_clones < 1 then
     jira_main.display_message("Number of clones must be a positive integer", vim.log.levels.ERROR)
     return
   end
-
   clone_issue(issue_key, num_clones)
 end
 
--- New command to clone issues
-vim.api.nvim_create_user_command('JiraClone', function(opts)
-  jira_clone_command(opts.fargs)
-end, { nargs = '+' })
+function M.setup()
+  -- Only set up the command once
+  if not M.is_setup then
+    vim.api.nvim_create_user_command('JiraClone', function(opts)
+      jira_clone_command(opts.fargs)
+    end, { nargs = '+' })
+    M.is_setup = true
+  end
+end
 
-return {
-  clone_issue = clone_issue
-}
+M.clone_issue = clone_issue
+
+return M
