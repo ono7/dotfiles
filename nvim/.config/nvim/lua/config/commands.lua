e618bf531dcf1bcb5356ab34828c797fdf0a4943
Lima, Jose <20385388+ono7@users.noreply.github.com>
2024-12-08 16:27:59 -0600

    fix Gen to use lan150

---
+++ nvim/.config/nvim/lua/config/commands.lua
@@ -0,0 +1,3 @@
+vim.api.nvim_create_user_command("T", function(opts)
+  vim.cmd("split | startinsert | terminal " .. table.concat(opts.fargs, " "))
+end, { nargs = "*", complete = "file" })
--- nvim/.config/nvim/lua/plugins/gen-ai.lua
+++ nvim/.config/nvim/lua/plugins/gen-ai.lua
@@ -1,21 +1,22 @@
 return {
   "David-Kunz/gen.nvim",
   opts = {
-    model = "qwen2.5-coder:32b-instruct-q4_K_M-lima", -- The default model to use.
+    model = "qwen2.5-coder:32b-large-ctx", -- The default model to use.
     quit_map = "q", -- set keymap to close the response window
     retry_map = "<c-r>", -- set keymap to re-send the current prompt
     accept_map = "<c-cr>", -- set keymap to replace the previous selection with the last result
-    host = "localhost", -- The host running the Ollama service.
+    host = "100.64.0.150", -- The host running the Ollama service.
     port = "11434", -- The port on which the Ollama service is listening.
     display_mode = "float", -- The display mode. Can be "float" or "split" or "horizontal-split".
-    show_prompt = false, -- Shows the prompt submitted to Ollama.
+    show_prompt = true, -- Shows the prompt submitted to Ollama.
     show_model = true, -- Displays which model you are using at the beginning of your chat session.
     no_auto_close = false, -- Never closes the window automatically.
     file = false, -- Write the payload to a temporary file to keep the command short.
     hidden = false, -- Hide the generation window (if true, will implicitly set `prompt.replace = true`), requires Neovim >= 0.10
-    init = function(options)
-      pcall(io.popen, "ollama serve > /dev/null 2>&1 &")
-    end,
+    -- init = function(options)
+    --   pcall(io.popen, "ollama serve > /dev/null 2>&1 &")
+    -- end,
+    init = nil,
     -- Function to initialize Ollama
     command = function(options)
       local body = { model = options.model, stream = true }
