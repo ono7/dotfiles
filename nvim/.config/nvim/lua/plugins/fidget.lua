c516255fed3504149ca5a208fc6ff1631b622a15
Lima, Jose <20385388+ono7@users.noreply.github.com>
2024-12-08 22:42:06 -0600

    updates

--- nvim/.config/nvim/lua/plugins/fidget.lua
+++ nvim/.config/nvim/lua/plugins/fidget.lua
@@ -64,7 +64,8 @@ return {
         poll_rate = 10, -- How frequently to update and render notifications
         filter = vim.log.levels.INFO, -- Minimum notifications level
         history_size = 128, -- Number of removed messages to retain in history
-        override_vim_notify = false, -- Automatically override vim.notify() with Fidget
+        -- TODO: (jlima) 2024-12-08 22:06 testing override
+        override_vim_notify = true, -- Automatically override vim.notify() with Fidget
         -- How to configure notification groups when instantiated
         configs = { default = require("fidget.notification").default_config },
         -- Conditionally redirect notifications to another backend
@@ -89,7 +90,7 @@ return {
 
         -- Options related to the notification window and buffer
         window = {
-          normal_hl = "Comment", -- Base highlight group in the notification window
+          normal_hl = "Label", -- Base highlight group in the notification window
           winblend = 0, -- Background color opacity in the notification window
           border = "none", -- Border around the notification window
           zindex = 45, -- Stacking priority of the notification window
