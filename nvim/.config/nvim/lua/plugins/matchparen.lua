2afbcd45b3949ac2c7b81b6924f997c76d813dd0
Lima, Jose <20385388+ono7@users.noreply.github.com>
2024-12-09 00:09:35 -0600

    updates

--- nvim/.config/nvim/lua/plugins/matchparen.lua
+++ nvim/.config/nvim/lua/plugins/matchparen.lua
@@ -2,9 +2,9 @@ return {
   "monkoose/matchparen.nvim",
   config = function()
     require("matchparen").setup({
-      on_startup = true, -- Should it be enabled by default
-      hl_group = "MatchParen", -- highlight group of the matched brackets
-      augroup_name = "matchparen", -- almost no reason to touch this unless there is already augroup with such name
+      on_startup = true,
+      hl_group = "MatchParen",
+      augroup_name = "matchparen",
       debounce_time = 20, -- debounce time in milliseconds for rehighlighting of brackets.
     })
   end,
