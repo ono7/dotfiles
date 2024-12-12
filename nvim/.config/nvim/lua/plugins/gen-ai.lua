c85fe03105cfea0b53c06c346785aee00fbfce75
Lima, Jose <20385388+ono7@users.noreply.github.com>
2024-12-11 23:26:59 -0600

    Update gen-ai plugin configuration to hide prompt and model display by default

--- nvim/.config/nvim/lua/plugins/gen-ai.lua
+++ nvim/.config/nvim/lua/plugins/gen-ai.lua
@@ -8,8 +8,8 @@ return {
     host = "100.64.0.150", -- The host running the Ollama service.
     port = "11434", -- The port on which the Ollama service is listening.
     display_mode = "float", -- The display mode. Can be "float" or "split" or "horizontal-split".
-    show_prompt = true, -- Shows the prompt submitted to Ollama.
-    show_model = true, -- Displays which model you are using at the beginning of your chat session.
+    show_prompt = false, -- Shows the prompt submitted to Ollama.
+    show_model = false, -- Displays which model you are using at the beginning of your chat session.
     no_auto_close = false, -- Never closes the window automatically.
     file = false, -- Write the payload to a temporary file to keep the command short.
     hidden = false, -- Hide the generation window (if true, will implicitly set `prompt.replace = true`), requires Neovim >= 0.10
@@ -57,7 +57,7 @@ return {
       port = "11434", -- The port on which the Ollama service is listening.
       display_mode = "float", -- The display mode. Can be "float" or "split" or "horizontal-split".
       show_prompt = false, -- Shows the prompt submitted to Ollama.
-      show_model = true, -- Displays which model you are using at the beginning of your chat session.
+      show_model = false, -- Displays which model you are using at the beginning of your chat session.
       no_auto_close = false, -- Never closes the window automatically.
       file = false, -- Write the payload to a temporary file to keep the command short.
       hidden = false, -- Hide the generation window (if true, will implicitly set `prompt.replace = true`), requires Neovim >= 0.10
@@ -74,11 +74,6 @@ return {
           .. options.port
           .. "/api/chat -d $body"
       end,
-      -- The command for the Ollama service. You can use placeholders $prompt, $model and $body (shellescaped).
-      -- This can also be a command string.
-      -- The executed command must return a JSON object with { response, context }
-      -- (context property is optional).
-      -- list_models = '<omitted lua function>', -- Retrieves a list of model names
       debug = false, -- Prints errors and the command which is run.
     })
   end,
