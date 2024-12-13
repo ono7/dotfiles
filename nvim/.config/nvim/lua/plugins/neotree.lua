c8647dfe6f9f9c7339568a2189bf6b1025e3bc26
Lima, Jose <20385388+ono7@users.noreply.github.com>
2024-12-12 20:56:03 -0600

    Rename barbecu.lua to barbecu.lua.bak and update neotree configuration for better file filtering and buffer handling

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -14,7 +14,21 @@ return {
   config = function()
     require("neo-tree").setup({
       window = { width = 25 },
-      buffers = { follow_current_file = { enabled = true } },
+      filesystem = {
+        filtered_items = {
+          visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+          hide_dotfiles = false,
+          hide_gitignored = true,
+        },
+        follow_current_file = { enabled = true },
+      },
+      buffers = {
+        follow_current_file = {
+          enabled = true, -- This will find and focus the file in the active buffer every time
+          --              -- the current file is changed while the tree is open.
+          leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+        },
+      },
     })
   end,
 }
