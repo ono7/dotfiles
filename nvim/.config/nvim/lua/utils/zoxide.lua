5d95ddbe33c5408cd86d2c396576475efb9768b3
Lima, Jose <20385388+ono7@users.noreply.github.com>
2024-12-15 13:27:07 -0600

    Update kitty background color and add ftplugin for kitty in nvim config

--- nvim/.config/nvim/lua/utils/zoxide.lua
+++ nvim/.config/nvim/lua/utils/zoxide.lua
@@ -5,12 +5,12 @@ function M.setup()
     return
   end
 
-  local pickers = require "telescope.pickers"
-  local finders = require "telescope.finders"
+  local pickers = require("telescope.pickers")
+  local finders = require("telescope.finders")
   local conf = require("telescope.config").values
-  local actions = require "telescope.actions"
-  local action_state = require "telescope.actions.state"
-  local builtin = require "telescope.builtin"
+  local actions = require("telescope.actions")
+  local action_state = require("telescope.actions.state")
+  local builtin = require("telescope.builtin")
 
   local function zoxide_cd()
     local handle = io.popen("zoxide query -ls")
@@ -23,43 +23,47 @@ function M.setup()
       table.insert(items, { score = tonumber(score), path = path })
     end
 
-    table.sort(items, function(a, b) return a.score > b.score end)
+    table.sort(items, function(a, b)
+      return a.score > b.score
+    end)
 
-    pickers.new({}, {
-      prompt_title = "Zoxide Directories",
-      finder = finders.new_table {
-        results = items,
-        entry_maker = function(entry)
-          return {
-            value = entry,
-            display = entry.path,
-            ordinal = entry.path,
-          }
+    pickers
+      .new({}, {
+        prompt_title = "Zoxide Directories",
+        finder = finders.new_table({
+          results = items,
+          entry_maker = function(entry)
+            return {
+              value = entry,
+              display = entry.path,
+              ordinal = entry.path,
+            }
+          end,
+        }),
+        sorter = conf.generic_sorter({}),
+        attach_mappings = function(prompt_bufnr, map)
+          actions.select_default:replace(function()
+            actions.close(prompt_bufnr)
+            local selection = action_state.get_selected_entry()
+            vim.cmd("lcd " .. selection.value.path)
+            print("Changed to directory: " .. selection.value.path)
+
+            -- Open Telescope find_files in the selected directory
+            -- vim.defer_fn(function()
+            --   builtin.find_files({
+            --     cwd = selection.value.path,
+            --     prompt_title = "Find Files in " .. selection.value.path,
+            --   })
+            -- end, 100)
+          end)
+          return true
         end,
-      },
-      sorter = conf.generic_sorter({}),
-      attach_mappings = function(prompt_bufnr, map)
-        actions.select_default:replace(function()
-          actions.close(prompt_bufnr)
-          local selection = action_state.get_selected_entry()
-          vim.cmd('lcd ' .. selection.value.path)
-          print("Changed to directory: " .. selection.value.path)
-
-          -- Open Telescope find_files in the selected directory
-          vim.defer_fn(function()
-            builtin.find_files({
-              cwd = selection.value.path,
-              prompt_title = "Find Files in " .. selection.value.path,
-            })
-          end, 100)
-        end)
-        return true
-      end,
-    }):find()
+      })
+      :find()
   end
 
-  vim.api.nvim_create_user_command('ZoxideCD', zoxide_cd, {})
-  vim.api.nvim_set_keymap('n', '<leader>z', ':ZoxideCD<CR>', { noremap = true, silent = true })
+  vim.api.nvim_create_user_command("ZoxideCD", zoxide_cd, {})
+  vim.api.nvim_set_keymap("n", "<leader>z", ":ZoxideCD<CR>", { noremap = true, silent = true })
 
   M.loaded = true
 end
