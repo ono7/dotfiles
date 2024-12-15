080966cba410f5ffde9e0e219a719126c5883014
Lima, Jose <20385388+ono7@users.noreply.github.com>
2024-12-15 13:05:51 -0600

    Migrate gitcommit and go ftplugin configurations from Vimscript to Lua

--- nvim/.config/nvim/after/ftplugin/gitcommit.vim
+++
@@ -1,2 +0,0 @@
-setlocal formatoptions-=t
-setlocal wrap
---
+++ nvim/.config/nvim/after/ftplugin/go.lua
@@ -0,0 +1,8 @@
+vim.opt_local.shiftwidth = 4
+vim.opt_local.tabstop = 4
+vim.opt_local.expandtab = true
+vim.opt_local.softtabstop = 4
+vim.opt_local.textwidth = 0
+vim.opt_local.wrap = false
+vim.opt_local.makeprg = "go test"
+vim.opt_local.suffixesadd:append(".go")
