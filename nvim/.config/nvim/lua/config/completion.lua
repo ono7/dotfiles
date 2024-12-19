f77bebdb674b9375ee0557111d48a8fe8e9d1260
Lima, Jose <20385388+ono7@users.noreply.github.com>
2024-12-18 23:48:01 -0600

    Update completion settings and reduce menu update time

--- nvim/.config/nvim/lua/config/completion.lua
+++ nvim/.config/nvim/lua/config/completion.lua
@@ -1,8 +1,10 @@
 -- Set completion options
 vim.opt.completeopt = { "menu", "longest" }
-vim.opt.complete:append({ "w", "b", "u", "t", "i" })
+-- vim.opt.complete:append({ "w", "b", "u", "t", "i" })
+vim.opt.complete:append({ "w", "b", "u", "t" })
 
--- Reduce completion menu delay
-vim.opt.updatetime = 120
+-- reduce completeopt menu time
+vim.opt.updatetime = 100
+
 -- Map Ctrl-Y to trigger completion and auto-select
 vim.api.nvim_set_keymap("i", "<C-y>", [[<C-n><C-n>]], { noremap = true, silent = true })
