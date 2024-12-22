00085b7867317bbf32122d5cd36878d24a1a9fef
Lima, Jose <20385388+ono7@users.noreply.github.com>
2024-12-22 13:11:17 -0600

    fix(go.lua): update makeprg and errorformat for Go files

--- nvim/.config/nvim/after/ftplugin/go.lua
+++ nvim/.config/nvim/after/ftplugin/go.lua
@@ -6,3 +6,8 @@ vim.opt_local.textwidth = 0
 vim.opt_local.wrap = false
 vim.opt_local.makeprg = "go test"
 vim.opt_local.suffixesadd:append(".go")
+
+vim.opt_local.makeprg = "go build -v ./..."
+-- with tests
+-- vim.opt_local.makeprg = 'go build -v ./... && go test -v ./...'
+vim.opt_local.errorformat = "%f:%l:%c: %m"
