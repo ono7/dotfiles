89fab6605c43d85423bff34b2e4adb24363b74b2
Lima, Jose <20385388+ono7@users.noreply.github.com>
2024-12-23 17:08:56 -0600

    fix(runner.lua): ensure non-empty data is appended and print job ID

--- nvim/.config/nvim/lua/utils/runner.lua
+++ nvim/.config/nvim/lua/utils/runner.lua
@@ -1,39 +1,32 @@
 local M = {}
-
 function M.setup()
   -- Check if already loaded
   if M.loaded then
     return
   end
-
   local term_size = 5
-
   vim.api.nvim_create_user_command("M", function(args)
     -- Create the buffer
     local terminal_buf = vim.api.nvim_create_buf(false, true)
-
     -- Create horizontal split at the bottom
     vim.cmd("belowright " .. term_size .. "split")
-
     -- Get the window ID of the new split and set its buffer
     local win_id = vim.api.nvim_get_current_win()
     vim.api.nvim_win_set_buf(win_id, terminal_buf)
-
     -- Set up the job output handling
     local append_data = function(_, data)
-      if data then
+      if data and data ~= "" then
         vim.api.nvim_buf_set_lines(terminal_buf, -1, -1, false, data)
       end
     end
-
-    vim.fn.jobstart(args.args, {
+    -- Store the job ID
+    local job_id = vim.fn.jobstart(args.args, {
       stdout_buffered = false,
       on_stdout = append_data,
       on_stderr = append_data,
     })
+    print("job: " .. job_id)
   end, { nargs = "*" })
-
   M.loaded = true
 end
-
 return M
