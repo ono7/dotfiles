6c78758d7b4c5f5b7408b986750533b0330766b6
Lima, Jose <20385388+ono7@users.noreply.github.com>
2024-12-29 00:35:03 -0600

    fix(alacritty, neovide): update configurations for theme and appearance, add transparent frame to neovide

--- nvim/.config/nvim/lua/config/neovide.lua
+++ nvim/.config/nvim/lua/config/neovide.lua
@@ -12,7 +12,8 @@ if vim.g.neovide then
   vim.g.neovide_scroll_animation_length = 0.00
   vim.g.neovide_cursor_animate_in_insert_mode = true
   vim.g.neovide_cursor_animate_command_line = false
-  vim.opt.guifont = "Iosevka Custom:h27:#e-subpixelantialias:#h-none"
+  vim.g.neovide_frame_no_title = true -- remove title bar
+  vim.opt.guifont = "Iosevka Custom:h26:#h-none"
 
   vim.opt.linespace = 4
 
