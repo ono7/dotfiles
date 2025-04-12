d7d28ad423f3d862ed2eef1e2f9b53f1581d08fe
Lima, Jose <20385388+ono7@users.noreply.github.com>
2025-04-12 02:04:58 -0500

    neovim: cleaup of treesitter.lua plugin config

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,20 +1,13 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    version = false,
-    enabled = true,
     build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
-    -- dependencies = {
-    --   "nvim-treesitter/nvim-treesitter-textobjects",
-    --   -- "windwp/nvim-ts-autotag", -- typescript/js tag closer
-    -- },
+    -- event = { "BufReadPost", "BufNewFile" },
     config = function()
-      -- this keeps this config from breaking when selecting up to "the end of this quote" and deleting
       -- this should be uncommented if using object motions
       -- vim.opt.selection = "exclusive"
       require("nvim-treesitter.configs").setup({
-        -- sync_install = false,
+        sync_install = true,
         -- modules = {},
         highlight = {
           enable = true,
@@ -56,42 +49,6 @@ return {
           "vim",
           "vimdoc",
           "yaml",
-        },
-        incremental_selection = {
-          enable = false,
-          keymaps = {},
-        },
-        textobjects = {
-          select = {
-            -- this should be set, if enabled = true here
-            -- vim.opt.selection = "exclusive"
-            enable = false,
-            -- lookahead = true,
-
-            keymaps = {
-              -- You can use the capture groups defined in textobjects.scm
-              ["af"] = { query = "@function.outer", desc = "around a function" },
-              ["if"] = { query = "@function.inner", desc = "inner part of a function" },
-              ["am"] = { query = "@function.outer", desc = "around a class" },
-              ["im"] = { query = "@class.inner", desc = "inner part of a class" },
-            },
-            selection_modes = {
-              ["@parameter.outer"] = "v", -- charwise
-              ["@parameter.inner"] = "v", -- charwise
-              ["@function.outer"] = "v", -- charwise
-              ["@conditional.outer"] = "V", -- linewise
-            },
-            include_surrounding_whitespace = false,
-          },
-          move = {
-            enable = false,
-            ["[p"] = { query = "@parameter.inner", desc = "Previous parameter" },
-            -- },
-            ["]p"] = { query = "@parameter.inner", desc = "Next parameter" },
-          },
-          swap = {
-            enable = false,
-          },
         },
       })
     end,
