c4d01a246464c3ebe745858b20d5f27a46355a26
Lima, Jose <20385388+ono7@users.noreply.github.com>
2025-05-12 21:48:56 -0500

    neovim: add file name to top status bar

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -6,6 +6,18 @@ vim.opt.shell = "zsh"
 
 vim.opt.shada = "'20,<1000,s100,:100,/100,h,r~/COMMIT_EDITMSG"
 
+-- local function branch_name()
+--   local branch = vim.fn.system("git branch --show-current 2> /dev/null | tr -d '\n'")
+--   if branch ~= "" then
+--     return branch
+--   else
+--     return ""
+--   end
+-- end
+--
+-- vim.opt.winbar = "%=%-.75F %-m {%{get(b:, 'branch_name', '')}}"
+vim.opt.winbar = "%=%-.75F %-m"
+
 vim.opt.fsync = false
 vim.opt.autochdir = false
 vim.opt.autoindent = true
