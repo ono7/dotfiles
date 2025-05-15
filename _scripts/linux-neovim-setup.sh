54d74b30d2e2faaf069fa794df58750ddb53768c
Lima, Jose <20385388+ono7@users.noreply.github.com>
2025-05-15 13:21:26 -0500

    updates linux nvim install url

--- _scripts/linux-neovim-setup.sh
+++ _scripts/linux-neovim-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 log() {
-    printf '\n%s - %s\n\n' "$(date '+%Y-%m-%d %H:%M:%S')" "$*"
+  printf '\n%s - %s\n\n' "$(date '+%Y-%m-%d %H:%M:%S')" "$*"
 }
 
 log "$0"
@@ -18,11 +18,12 @@ rm -f ~/local/bin/nvim
 rm -f ~/local/bin/shortpath
 rm -rf ~/nvim-linux64.*
 log "downloading neovim"
-curl -sL -O https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
-[[ ! -f nvim-linux64.tar.gz ]] && log "failed to download neovim"
-tar xzf nvim-linux64.tar.gz
-mv nvim-linux64 nvim
-rm nvim-linux64.*
+# curl -sL -O https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
+curl -sL -O https://github.com/neovim/neovim-releases/releases/download/v0.11.1/nvim-linux-x86_64.tar.gz
+[[ ! -f nvim-linux-x86_64.tar.gz ]] && log "failed to download neovim"
+tar xzf nvim-linux-x86_64.tar.gz
+mv nvim-linux-x86_64 nvim
+rm nvim-linux.*
 mkdir -p "$HOME/local/bin"
 ln -sf ~/nvim/bin/nvim ~/local/bin/nvim
 
