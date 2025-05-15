14bc1c7008a9004a8eb27fb5fb0821b64e647367
Lima, Jose <20385388+ono7@users.noreply.github.com>
2025-05-15 11:32:20 -0500

    updates

--- _scripts/setup_linux_deps.sh
+++ _scripts/setup_linux_deps.sh
@@ -28,7 +28,7 @@ sudo apt install python3 python3.12 python3.12-pip python3.12-venv socat -y
 # install python3.11
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt update
-sudo apt install python3.11
+sudo apt install python3.11 -y
 
 if type snap &>/dev/null; then
   log "installing snap packages"
