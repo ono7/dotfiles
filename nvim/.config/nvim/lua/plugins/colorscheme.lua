8b374156b928893a075e0d174ec923f2332fccda
Lima, Jose <20385388+ono7@users.noreply.github.com>
2025-05-24 07:51:52 -0500

    updates

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -113,6 +113,8 @@ return {
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "cmpBorder", { fg = "#313244", bold = true })
     vim.api.nvim_set_hl(0, "FloatBorder", { link = "Comment", bold = true })
+    vim.api.nvim_set_hl(0, "DiagnosticUnnecessary", { fg = "#4e5574" })
+
     vim.api.nvim_set_hl(0, "cmpDoc", {})
 
     -- Enhanced Diagnostic highlights with dark background matching Alacritty
