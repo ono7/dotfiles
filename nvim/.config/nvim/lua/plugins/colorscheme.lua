7371af881c0d3e7623194f6c0d05c5c51db5c332
Lima, Jose <20385388+ono7@users.noreply.github.com>
2025-07-03 09:24:57 -0500

    updates

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -181,7 +181,7 @@ return {
     vim.api.nvim_set_hl(0, "@nospell.markdown_inline", { fg = mycolors.blue })
     vim.api.nvim_set_hl(0, "Boolean", {})
     vim.api.nvim_set_hl(0, "Repeat", { fg = mycolors.red, bold = false })
-    vim.api.nvim_set_hl(0, "@keyword.function", { fg = mycolors.red, italic = false, bold = false })
+    vim.api.nvim_set_hl(0, "@keyword.function", { fg = mycolors.red, italic = true, bold = false })
     vim.api.nvim_set_hl(0, "@keyword.return", { fg = mycolors.red })
     vim.api.nvim_set_hl(0, "Conditional", { fg = mycolors.red, bold = false })
     vim.api.nvim_set_hl(0, "Include", { fg = mycolors.red, bold = true })
