1cbcd2f37f84ff6b2975c3ce9cfc4111b4aef47e
Lima, Jose <20385388+ono7@users.noreply.github.com>
2025-07-10 15:49:28 -0500

    add autotiling to linux deps

--- _scripts/setup_linux_deps.sh
+++ _scripts/setup_linux_deps.sh
@@ -30,7 +30,7 @@ install_arch_packages() {
   sudo pacman -S --needed --noconfirm docker foot
   sudo pacman -S --needed --noconfirm openssl tree zsh the_silver_searcher \
     fd unzip xclip ripgrep stow make sqlite wget shfmt shellcheck \
-    rlwrap pass
+    rlwrap pass autotiling
 
   temp1=$PWD
   git clone https://aur.archlinux.org/yay-bin.git ~/yay
