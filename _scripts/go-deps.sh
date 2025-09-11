df8dc16d800cc3e0b58439818a53659a87ff0367
Lima, Jose <20385388+ono7@users.noreply.github.com>
2025-09-11 13:35:43 -0500

    add dlv to godeps.sh

--- _scripts/go-deps.sh
+++ _scripts/go-deps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 log() {
-    printf '\n%s - %s\n\n' "$(date '+%Y-%m-%d %H:%M:%S')" "$*"
+  printf '\n%s - %s\n\n' "$(date '+%Y-%m-%d %H:%M:%S')" "$*"
 }
 
 log "$0"
@@ -10,6 +10,7 @@ log "$0"
 if type go &>/dev/null; then
   go install -v github.com/incu6us/goimports-reviser/v3@latest
   go install golang.org/x/tools/cmd/goimports@latest
+  go install github.com/go-delve/delve/cmd/dlv@latest
   go install github.com/shurcooL/markdownfmt@latest
   go install github.com/moorereason/mdfmt@latest
   go install github.com/fatih/gomodifytags@latest
