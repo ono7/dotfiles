cf154e438acaa66ec3fd9a97fb426fe50cbb1479
Lima, Jose <20385388+ono7@users.noreply.github.com>
2025-09-15 11:59:58 -0500

    update keymaps to use c-1, 2, 3, 4, etc

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -1,3 +1,4 @@
+opts = { noremap = true, silent = true }
 return {
   "ThePrimeagen/harpoon",
   branch = "harpoon2",
@@ -15,23 +16,23 @@ return {
 
     vim.keymap.set("n", "ma", function()
       harpoon:list():add()
-    end)
+    end, opts)
     vim.keymap.set("n", "mm", function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
-    end)
+    end, opts)
 
-    vim.keymap.set("n", "<M-1>", function()
+    vim.keymap.set("n", "<C-1>", function()
       harpoon:list():select(1)
-    end)
-    vim.keymap.set("n", "<M-2>", function()
+    end, opts)
+    vim.keymap.set("n", "<C-2>", function()
       harpoon:list():select(2)
-    end)
-    vim.keymap.set("n", "<M-3>", function()
+    end, opts)
+    vim.keymap.set("n", "<C-3>", function()
       harpoon:list():select(3)
-    end)
-    vim.keymap.set("n", "<M-4>", function()
+    end, opts)
+    vim.keymap.set("n", "<C-4>", function()
       harpoon:list():select(4)
-    end)
+    end, opts)
 
     -- Toggle previous & next buffers stored within Harpoon list
     -- vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
