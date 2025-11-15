68e376fc987519cd3c92a146bffb666b5294eb1a
Lima, Jose <20385388+ono7@users.noreply.github.com>
2025-11-14 19:17:29 -0600

    updates

--- nvim/.config/nvim/lua/plugins/lsp/snippets.lua
+++ nvim/.config/nvim/lua/plugins/lsp/snippets.lua
@@ -3,27 +3,6 @@ return {
   "dcampos/nvim-snippy",
   opts = {},
   config = function()
-    -- local snippy = require("snippy")
-    -- snippy.setup({
-    --   snippet_dirs = "~/.config/nvim/snippets",
-    --   mappings = {
-    --     is = {
-    --       ["<Tab>"] = "expand_or_advance",
-    --       ["<S-Tab>"] = "previous",
-    --     },
-    --     x = {
-    --       ["<leader>d"] = "cut_text",
-    --     },
-    --   },
-    -- })
-
-    -- local snippy_ok, snippy = pcall(require, "snippy")
-    --
-    -- if not snippy_ok then
-    --   print("Error in pcall snippy -> ~/.config/nvim/lua/plugins/lsp/cmp.lua")
-    --   return
-    -- end
-
     require("snippy").setup({
       snippet_dirs = "~/.config/nvim/snippets",
       mappings = {
