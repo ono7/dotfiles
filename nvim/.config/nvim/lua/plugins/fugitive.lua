2f86852ba4a2893c59a456698af75a1e01baf054
Lima, Jose <20385388+ono7@users.noreply.github.com>
2025-11-15 23:50:07 -0600

    fix some issues with fugitive

--- nvim/.config/nvim/lua/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -3,11 +3,11 @@ return {
   config = function()
     vim.cmd([[hi! diffAdded ctermfg=188 ctermbg=64 cterm=bold guifg=#50FA7B guibg=NONE gui=bold]])
     vim.cmd([[hi! diffRemoved ctermfg=88 ctermbg=NONE cterm=NONE guifg=#FA5057 guibg=NONE gui=NONE]])
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = "fugitive",
-      callback = function()
-        vim.opt_local.cmdheight = 2
-      end,
-    })
+    -- vim.api.nvim_create_autocmd("FileType", {
+    --   pattern = "fugitive",
+    --   callback = function()
+    --     vim.opt_local.cmdheight = 2
+    --   end,
+    -- })
   end,
 }
