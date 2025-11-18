960703f28de6a052a88bfc592975bf7f7739ade9
Lima, Jose <20385388+ono7@users.noreply.github.com>
2025-11-18 09:14:24 -0600

    mini-sessions, do not save term buffers

--- nvim/.config/nvim/lua/plugins/mini-sessions.lua
+++ nvim/.config/nvim/lua/plugins/mini-sessions.lua
@@ -18,6 +18,11 @@ return {
     -- Save clean session on exit
     vim.api.nvim_create_autocmd("VimLeavePre", {
       callback = function()
+        for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+          if vim.bo[buf].buftype == "terminal" then
+            pcall(vim.api.nvim_buf_delete, buf, { force = true })
+          end
+        end
         -- 🔥 delete arglist so no stale files get written into the session
         vim.cmd("argglobal")
         vim.cmd("%argdel")
