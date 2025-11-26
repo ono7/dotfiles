a4722d1ee606ee466f8ececd75f65edd6ad000b9
Lima, Jose <20385388+ono7@users.noreply.github.com>
2025-11-26 11:20:10 -0600

    add feat fetch all in .gitconfig

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -57,6 +57,9 @@ vim.opt.undofile = true
 vim.opt.undolevels = 1000
 vim.opt.undoreload = 10000
 
+-- single width icons/font
+vim.opt.ambiwidth = "single"
+
 -- Create undo directory if it doesn't exist
 local undo_dir = vim.fn.expand(vim.env.HOME .. "/.undo")
 if vim.fn.isdirectory(undo_dir) == 0 then
@@ -142,7 +145,7 @@ endfunction
 nnoremap <silent> gz :call ToggleFolding()<CR>
 ]])
 
-vim.g.markdown_folding = 1    -- enable markdown folding
+vim.g.markdown_folding = 1 -- enable markdown folding
 
 vim.opt.formatoptions = "qlj" -- TODO: overwritten in my_cmds.lua
 
@@ -223,13 +226,13 @@ vim.opt.splitright = true
 vim.opt.splitbelow = false
 vim.opt.splitkeep = "screen"
 vim.opt.swapfile = false
-vim.opt.synmaxcol = 200          -- for performace
+vim.opt.synmaxcol = 200 -- for performace
 vim.opt.tags = [[./tags,tags;~]] -- search upwards until ~ (homedir)
 vim.opt.textwidth = 80
-vim.opt.timeout = false          -- remove timeout for partially typed commands
+vim.opt.timeout = false -- remove timeout for partially typed commands
 vim.opt.timeoutlen = 300
 vim.opt.title = true
 vim.opt.titlestring = ""
-vim.opt.lazyredraw = true    -- Don't redraw during macros
-vim.opt.updatetime = 250     -- Faster CursorHold events
+vim.opt.lazyredraw = true -- Don't redraw during macros
+vim.opt.updatetime = 250 -- Faster CursorHold events
 vim.opt.smoothscroll = false -- disable for performance
