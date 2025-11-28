84c8ec0e2b6cb44446034479dc20be5aa94893fd
Lima, Jose <20385388+ono7@users.noreply.github.com>
2025-11-27 20:35:50 -0600

    fidget.lua: fixed default color group to match Normal

--- nvim/.config/nvim/lua/plugins/fidget.lua
+++ nvim/.config/nvim/lua/plugins/fidget.lua
@@ -92,16 +92,16 @@ return {
         -- Options related to the notification window and buffer
         window = {
           -- normal_hl = "Label", -- Base highlight group in the notification window
-          normal_hl = "DiffAdd", -- Base highlight group in the notification window
-          winblend = 0,          -- Background color opacity in the notification window
-          border = "none",       -- Border around the notification window
-          zindex = 45,           -- Stacking priority of the notification window
-          max_width = 0,         -- Maximum width of the notification window
-          max_height = 0,        -- Maximum height of the notification window
-          x_padding = 0,         -- Padding from right edge of window boundary
-          y_padding = 0,         -- Padding from bottom edge of window boundary
-          align = "bottom",      -- How to align the notification window "top, bottom"
-          relative = "editor",   -- What the notification window position is relative to
+          normal_hl = "Normal", -- Base highlight group in the notification window
+          winblend = 0,         -- Background color opacity in the notification window
+          border = "none",      -- Border around the notification window
+          zindex = 45,          -- Stacking priority of the notification window
+          max_width = 0,        -- Maximum width of the notification window
+          max_height = 0,       -- Maximum height of the notification window
+          x_padding = 0,        -- Padding from right edge of window boundary
+          y_padding = 0,        -- Padding from bottom edge of window boundary
+          align = "bottom",     -- How to align the notification window "top, bottom"
+          relative = "editor",  -- What the notification window position is relative to
         },
       },
 
