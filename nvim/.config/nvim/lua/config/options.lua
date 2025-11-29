4c30082779f801efc318798b82265ddd6dbd54af
Lima, Jose <20385388+ono7@users.noreply.github.com>
2025-11-29 12:09:23 -0600

    options.lua: updates

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -145,7 +145,7 @@ endfunction
 nnoremap <silent> gz :call ToggleFolding()<CR>
 ]])
 
-vim.g.markdown_folding = 1 -- enable markdown folding
+vim.g.markdown_folding = 1    -- enable markdown folding
 
 vim.opt.formatoptions = "qlj" -- TODO: overwritten in my_cmds.lua
 
@@ -200,10 +200,12 @@ vim.opt.nrformats = "bin,hex,alpha"
 
 vim.opt.numberwidth = 1
 vim.opt.signcolumn = "yes:1"
-vim.opt.statuscolumn = "%=%{v:relnum?v:relnum:v:lnum}%s"
+-- vim.opt.statuscolumn = "%=%{v:relnum?v:relnum:v:lnum}%s"
 vim.opt.pumheight = 5
-vim.opt.relativenumber = true
-vim.opt.number = true
+vim.opt.relativenumber = false
+vim.opt.number = false
+vim.opt.laststatus = 0
+vim.opt.ruler = true
 vim.opt.complete = ".,w,b"
 vim.opt.shortmess = "aoOstTWICc" -- F dont show file info when editing file, useful when statusline is enabled already
 vim.opt.showbreak = [[↪ ]]
@@ -226,13 +228,13 @@ vim.opt.splitright = true
 vim.opt.splitbelow = false
 vim.opt.splitkeep = "screen"
 vim.opt.swapfile = false
-vim.opt.synmaxcol = 200 -- for performace
+vim.opt.synmaxcol = 200          -- for performace
 vim.opt.tags = [[./tags,tags;~]] -- search upwards until ~ (homedir)
 vim.opt.textwidth = 80
-vim.opt.timeout = false -- remove timeout for partially typed commands
+vim.opt.timeout = false          -- remove timeout for partially typed commands
 vim.opt.timeoutlen = 300
 vim.opt.title = true
 vim.opt.titlestring = ""
-vim.opt.lazyredraw = true -- Don't redraw during macros
-vim.opt.updatetime = 250 -- Faster CursorHold events
+vim.opt.lazyredraw = true    -- Don't redraw during macros
+vim.opt.updatetime = 250     -- Faster CursorHold events
 vim.opt.smoothscroll = false -- disable for performance
