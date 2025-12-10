23b2f6d3b3faa70cbebd9a3da306650df49b23b7
Lima, Jose <20385388+ono7@users.noreply.github.com>
2025-12-10 08:25:07 -0600

    tiny-inline-diagnostics.lua: disable enable_on_insert

--- nvim/.config/nvim/lua/plugins/tiny-inline-diagnostics.lua
+++ nvim/.config/nvim/lua/plugins/tiny-inline-diagnostics.lua
@@ -1,11 +1,12 @@
 return {
   "rachartier/tiny-inline-diagnostic.nvim",
   event = "VeryLazy", -- Or `LspAttach`
-  priority = 1000,    -- needs to be loaded in first
+  priority = 1000, -- needs to be loaded in first
   config = function()
     require("tiny-inline-diagnostic").setup({
       preset = "classic",
       transparent_bg = true, -- Set
+      enable_on_insert = false,
       transparent_cursorline = true,
       throttle = 200,
       hi = {
