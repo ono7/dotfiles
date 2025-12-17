90ac2309be3db8999a748d327296c00989003882
Lima, Jose <20385388+ono7@users.noreply.github.com>
2025-12-17 15:55:11 -0600

    updates

---
+++ nvim/.config/nvim/lua/plugins/vim-expand-region.lua
@@ -0,0 +1,8 @@
+-- Lazy.nvim
+return {
+  "terryma/vim-expand-region",
+  keys = {
+    { "v", "<Plug>(expand_region_expand)", mode = "v", desc = "Expand Region" },
+    { "<C-v>", "<Plug>(expand_region_shrink)", mode = "v", desc = "Shrink Region" },
+  },
+}
