6698b7de898074024ef0069ef9d059f7a086941b
Lima, Jose <20385388+ono7@users.noreply.github.com>
2025-12-19 12:25:19 -0600

    neovide.lua: updates

--- nvim/.config/nvim/lua/config/neovide.lua
+++ nvim/.config/nvim/lua/config/neovide.lua
@@ -32,8 +32,9 @@ if vim.g.neovide then
   local is_wsl = uname.sysname == "Linux" and uname.release:lower():match("microsoft")
 
   -- this adds too much padding, but its a way to define font size based on OS
-  -- if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 or is_wsl then
-  --   vim.opt.guifont = "Iosevka Custom:Medium Extended,Bold Extended,Medium Extended Italic:h14"
+  if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 or is_wsl then
+    vim.opt.guifont = "Iosevka Custom:Medium Extended,Bold Extended,Medium Extended Italic:h14"
+  end
   -- else
   --   vim.opt.guifont = "Iosevka Custom:Medium Extended,Bold Extended,Medium Extended Italic:h24"
   -- end
