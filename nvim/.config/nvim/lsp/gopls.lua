e66a820e1151c54ced21e0cf31988b9596a78954
Lima, Jose <20385388+ono7@users.noreply.github.com>
2026-01-15 19:37:40 -0500

    gopls.lua: updates to go pls

--- nvim/.config/nvim/lsp/gopls.lua
+++ nvim/.config/nvim/lsp/gopls.lua
@@ -1,5 +1,16 @@
 return {
   cmd = { "gopls" },
-  filetypes = { "go" },
+  filetypes = { "go", "gomod", "gowork", "gotmpl" },
   root_markers = { "go.mod", "go.work", ".git" },
+  settings = {
+    gopls = {
+      completeUnimported = true,
+      usePlaceholders = true,
+      analyses = {
+        unusedparams = true,
+      },
+      staticcheck = true,
+      gofumpt = true, -- Even if using conform, enabling this helps gopls' internal logic
+    },
+  },
 }
