205b67b548e79c43e63cfe97ed4ded75842a9705
Lima, Jose <20385388+ono7@users.noreply.github.com>
2026-04-01 00:48:33 -0500

    tree sitter, install all parsers

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,119 +1,61 @@
-return {}
--- return {
---   {
---     "nvim-treesitter/nvim-treesitter",
---     event = { "BufReadPost", "BufNewFile" },
---     -- lazy = false,
---     branch = "main",
---     version = false,
---     build = ":TSUpdate",
---     dependencies = { "RRethy/nvim-treesitter-endwise" },
---     config = function()
---       local ts = require("nvim-treesitter")
---       local ts_cfg = require("nvim-treesitter.config")
---       local parsers = require("nvim-treesitter.parsers")
---
---       local ensure_installed = {
---         "bash",
---         "c",
---         "cmake",
---         "comment",
---         "css",
---         "diff",
---         "dockerfile",
---         "git_config",
---         "git_rebase",
---         "gitcommit",
---         "gitignore",
---         "go",
---         "gomod",
---         "gosum",
---         "gotmpl",
---         "gowork",
---         "groovy",
---         "hcl",
---         "html",
---         "javascript",
---         "jsdoc",
---         "json",
---         --"jsonnet",
---         -- "json5", -- https://json5.org
---         "just",
---         "lua",
---         "luadoc",
---         "markdown",
---         "markdown_inline",
---         "printf",
---         "python",
---         "query",
---         "regex",
---         "ruby",
---         "rust",
---         "sql",
---         "terraform",
---         "tmux",
---         "toml",
---         "typescript",
---         "vim",
---         "vimdoc",
---         "xml",
---         "yaml",
---         "zig",
---         "zsh",
---       }
---       local installed = ts_cfg.get_installed()
---       local to_install = vim
---         .iter(ensure_installed)
---         :filter(function(parser)
---           return not vim.tbl_contains(installed, parser)
---         end)
---         :totable()
---
---       if #to_install > 0 then
---         ts.install(to_install)
---       end
---
---       local ignore_filetype = {
---         "checkhealth",
---         "lazy",
---         "mason",
---         "snacks_dashboard",
---         "snacks_notif",
---         "snacks_win",
---         "snacks_input",
---         "snacks_picker_input",
---         "TelescopePrompt",
---         "alpha",
---         "dashboard",
---         "spectre_panel",
---         "NvimTree",
---         "undotree",
---         "Outline",
---         "sagaoutline",
---         "copilot-chat",
---         "vscode-diff-explorer",
---       }
---
---       local group = vim.api.nvim_create_augroup("TreesitterSetup", { clear = true })
---
---       vim.api.nvim_create_autocmd("FileType", {
---         group = group,
---         desc = "Enable TreeSitter highlighting and indentation",
---         callback = function(ev)
---           local ft = ev.match
---
---           if vim.tbl_contains(ignore_filetype, ft) then
---             return
---           end
---
---           local lang = vim.treesitter.language.get_lang(ft) or ft
---           local buf = ev.buf
---           pcall(vim.treesitter.start, buf, lang)
---
---           vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
---           vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
---         end,
---       })
---     end,
---   },
--- }
+return {
+  "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  branch = "main",
+  build = ":TSUpdate",
+  config = function()
+    -- Custom registrations
+    vim.filetype.add({
+      extension = {
+        csproj = "xml",
+        esproj = "xml",
+        keymap = "c",
+        mdx = "markdown",
+        uproject = "json",
+        wsdl = "xml",
+      },
+    })
+
+    -- Treesitter directory
+    local treesitter_dir = vim.fn.stdpath("data") .. "/lazy/nvim-treesitter/"
+
+    -- Collect all available parsers
+    local parsers = {}
+    for name, type in vim.fs.dir(treesitter_dir .. "runtime/queries") do
+      if type == "directory" then
+        table.insert(parsers, name)
+      end
+    end
+
+    -- Install file type parsers
+    require("nvim-treesitter").install(parsers)
+
+    -- Register known file types
+    dofile(treesitter_dir .. "plugin/filetypes.lua")
+
+    -- Get file types
+    local file_types = vim
+      .iter(parsers)
+      :map(function(parser)
+        return vim.treesitter.language.get_filetypes(parser)
+      end)
+      :flatten()
+      :totable()
+
+    -- Auto-run
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = file_types,
+      callback = function(args)
+        -- Highlights
+        vim.treesitter.start()
+
+        -- Folds
+        vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+        vim.wo[0][0].foldmethod = "expr"
+
+        -- Indentation
+        vim.bo[args.buf].indentexpr = 'v:lua.require"nvim-treesitter".indentexpr()'
+      end,
+    })
+  end,
+}
