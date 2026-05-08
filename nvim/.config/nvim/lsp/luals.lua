fbeda5e505ffd6565d2de6f831fcb79a222fe024
Lima, Jose <20385388+ono7@users.noreply.github.com>
2026-05-08 13:52:27 -0500

    luals.lua: updates

--- nvim/.config/nvim/lsp/luals.lua
+++ nvim/.config/nvim/lsp/luals.lua
@@ -2,23 +2,26 @@ return {
   cmd = { "lua-language-server" },
   filetypes = { "lua" },
   root_markers = { ".luarc.json", ".luarc.jsonc", ".git" },
-  telemetry = { enabled = false },
-  formatters = {
-    ignoreComments = false,
-  },
   settings = {
     Lua = {
       runtime = {
         version = "LuaJIT",
       },
+      diagnostics = {
+        -- This fixes the "undefined global vim" error
+        globals = { "vim" },
+      },
       workspace = {
+        -- Only include the Neovim runtime and specifically your config
+        -- Avoid using vim.api.nvim_get_runtime_file("", true) as it is too broad
         library = {
-          vim.env.VIMRUNTIME,
-          -- vim.api.nvim_get_runtime_file("", true),
+          [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+          [vim.fn.stdpath("config") .. "/lua"] = true,
         },
-        -- library = vim.api.nvim_get_runtime_file("", true),
-        maxPreload = 10000,
-        preloadFileSize = 10000,
+        maxPreload = 2000,
+        preloadFileSize = 1000,
+        -- Prevents the LSP from asking to configure your workspace every time
+        checkThirdParty = false,
       },
       telemetry = { enable = false },
     },
