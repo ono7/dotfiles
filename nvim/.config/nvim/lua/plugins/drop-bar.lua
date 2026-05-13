859cea78509ee8befe788eb25e007a61b67375d7
Lima, Jose <20385388+ono7@users.noreply.github.com>
2026-05-13 08:06:14 -0500

    configure other settings for bread crums

--- nvim/.config/nvim/lua/plugins/drop-bar.lua
+++ nvim/.config/nvim/lua/plugins/drop-bar.lua
@@ -4,13 +4,27 @@ return {
     "nvim-telescope/telescope-fzf-native.nvim",
     build = "make",
   },
-  -- Ensure it loads immediately if you want it visible on all buffers
   lazy = false,
   config = function()
     local dropbar = require("dropbar")
     local dropbar_api = require("dropbar.api")
 
     dropbar.setup({
+      bar = {
+        sources = function(buf, _)
+          local sources = require("dropbar.sources")
+          local utils = require("dropbar.utils")
+
+          -- Filter the path source to only show the file name
+          return {
+            utils.source.fallback({
+              sources.lsp,
+              sources.treesitter,
+              sources.markdown,
+            }),
+          }
+        end,
+      },
       icons = {
         kinds = {
           symbols = {
@@ -22,7 +36,6 @@ return {
       },
     })
 
-    -- Define keymaps after setup
     vim.keymap.set("n", "<Leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
     vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
     vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Select next context" })
