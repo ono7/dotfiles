3e35d2b8d194c71407231a361736c0dec757caf4
Lima, Jose <20385388+ono7@users.noreply.github.com>
2026-05-28 14:57:13 -0500

    updates

--- nvim/.config/nvim/lua/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -4,6 +4,11 @@ return {
     vim.cmd([[hi! diffAdded ctermfg=188 ctermbg=64 cterm=bold guifg=#50FA7B guibg=NONE gui=bold]])
     vim.cmd([[hi! diffRemoved ctermfg=88 ctermbg=NONE cterm=NONE guifg=#FA5057 guibg=NONE gui=NONE]])
 
+    vim.keymap.set("n", "<leader>g", "<cmd>G<CR>", {
+      silent = true,
+      desc = "Launch fugitive",
+    })
+
     vim.api.nvim_create_autocmd("FileType", {
       pattern = "fugitive",
       callback = function(event)
