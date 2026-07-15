ffcd3602a175a6cc79108eac9245cce8bd1e52ad
Lima, Jose <20385388+ono7@users.noreply.github.com>
2026-07-15 11:26:39 -0500

    colorscheme-ayu.lua: update

--- nvim/.config/nvim/lua/plugins/colorscheme-ayu.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme-ayu.lua
@@ -190,13 +190,6 @@ else
       vim.api.nvim_set_hl(0, "@text.danger", { link = "ErrorMsg" })
 
       vim.api.nvim_set_hl(0, "@text.note", { link = "Normal" })
-      vim.cmd([[
-        if exists("g:neovide")
-          hi TabLineSel guifg=#BEBEBC guibg=#233045 gui=bold
-          hi TabLine guifg=#5F6C77 guibg=#1A2330 gui=none
-          hi TabLineFill guibg=#151F2D guifg=NONE gui=none
-        endif
-      ]])
     end,
   }
 end
