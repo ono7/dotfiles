ed86cc7607a3d935212e86c22e91554c5259ea90
Lima, Jose <20385388+ono7@users.noreply.github.com>
2026-08-14 00:21:22 -0500

    Set button to red

--- nvim/.config/nvim/lua/plugins/diffview.lua
+++ nvim/.config/nvim/lua/plugins/diffview.lua
@@ -2,4 +2,36 @@ return {
   "sindrets/diffview.nvim",
   event = "VeryLazy",
   cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
+
+  -- Global keymaps to launch or close Diffview from anywhere in Neovim
+  keys = {
+    { "<leader>do", "<cmd>DiffviewOpen<cr>", desc = "Diffview Open" },
+    { "<leader>dc", "<cmd>DiffviewClose<cr>", desc = "Diffview Close" },
+  },
+
+  opts = function()
+    local actions = require("diffview.actions")
+
+    return {
+      enhanced_diff_hl = true, -- Highlights slightly better than default Neovim
+
+      view = {
+        -- Default view configuration
+        merge_tool = {
+          -- You can configure the layout here (e.g., "diff3_mixed" or "diff4_mixed")
+          layout = "diff3_mixed",
+        },
+      },
+
+      -- Explicitly map the conflict resolution keys for the merge tool
+      keymaps = {
+        merge_tool = {
+          { "n", "co", actions.conflict_choose("ours"),   { desc = "Choose OURS" } },
+          { "n", "ct", actions.conflict_choose("theirs"), { desc = "Choose THEIRS" } },
+          { "n", "ca", actions.conflict_choose("all"),    { desc = "Choose ALL" } },
+          { "n", "dx", actions.conflict_choose("none"),   { desc = "Choose NONE" } },
+        }
+      }
+    }
+  end,
 }
