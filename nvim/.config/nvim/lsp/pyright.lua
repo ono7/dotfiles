bdac4ebe05396af9c10c6d4421ded0731425abe5
Lima, Jose <20385388+ono7@users.noreply.github.com>
2026-08-27 20:57:02 -0500

    pyright.lua: update

--- nvim/.config/nvim/lsp/pyright.lua
+++ nvim/.config/nvim/lsp/pyright.lua
@@ -74,7 +74,7 @@ return {
         settings = client.config.settings,
       })
       -- vim.notify("VENV" .. trim_path(python_path))
-      vim.notify("env: ✓")
+      vim.notify("env: ✔")
     end
 
     -- 2. UX Optimization: Enable Inlay Hints (Neovim 0.10+)
