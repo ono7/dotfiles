7e6d8ab346416715af67972d7bc7257144b61110
Lima, Jose <20385388+ono7@users.noreply.github.com>
2026-08-28 11:52:36 -0500

    build-neovim.sh: update

--- _scripts/build-neovim.sh
+++ _scripts/build-neovim.sh
@@ -155,8 +155,6 @@ echo "Neovim built for platform ${ARCH}"
 echo "With compiler CFLAGS: ${CFLAGS}"
 echo ""
 
-cleanup
-
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
