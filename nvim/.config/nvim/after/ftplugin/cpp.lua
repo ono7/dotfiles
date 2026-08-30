00433e754b2b6a17e082b28e724cdc70d81fc19d
Lima, Jose <20385388+ono7@users.noreply.github.com>
2026-08-30 15:26:27 -0500

    cpp.lua, updatae

--- nvim/.config/nvim/after/ftplugin/cpp.lua
+++ nvim/.config/nvim/after/ftplugin/cpp.lua
@@ -4,7 +4,7 @@
 -- :Dispatch g++ % -o %< && ./%<
 
 -- for standard programs
-vim.b.dispatch = "g++ % -o %:r && ./%:r"
+vim.b.dispatch = "g++ % -o %:r -g -O0 && ./%:r"
 
 -- Keeps the shell open so you can see output/errors and prevents early buffer destruction
 -- vim.b.start = "cd %:p:h:S && g++ %:t:S -o %:t:r:S && ./%:t:r:S; exec $SHELL"
