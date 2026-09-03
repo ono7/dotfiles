c19bec7b73917a821659a662470faf77876def60
Lima, Jose <20385388+ono7@users.noreply.github.com>
2026-09-03 14:01:37 -0500

    gopher.lua, add

--- nvim/.config/nvim/lua/plugins/gopher.lua
+++ nvim/.config/nvim/lua/plugins/gopher.lua
@@ -3,10 +3,24 @@ return {
   ft = "go",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    -- "nvim-treesitter/nvim-treesitter",
+    "nvim-treesitter/nvim-treesitter",
   },
-  config = function()
+  -- Explicitly register commands so they can trigger lazy loading or autocomplete
+  cmd = {
+    "GoInstallDeps",
+    "GoUpdateDeps",
+    "GoTagAdd",
+    "GoTagRm",
+    "GoIfErr",
+    "GoTestAdd",
+    "GoTestsAll",
+    "GoMod",
+  },
+  opts = {},
+  build = function()
+    -- Load the plugin and run setup before invoking the installation command
+    vim.cmd.packadd("gopher.nvim")
     require("gopher").setup()
+    vim.cmd("GoInstallDeps")
   end,
-  build = ":silent! GoInstallDeps",
 }
