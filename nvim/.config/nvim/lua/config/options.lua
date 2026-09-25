461ed0700a42a4bd7f149f916edffacf9702e5f3
Lima, Jose <20385388+ono7@users.noreply.github.com>
2026-09-25 08:40:32 -0500

    options.lua, update

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -73,6 +73,7 @@ vim.opt.shada = "'100,<50,s10,:1000,/1000,h,r/COMMIT_EDITMSG,r/git-rebase-todo,!
 -- 7. System, Paths & File Handling
 -- =============================================================================
 vim.opt.shell = "zsh"
+vim.opt.shellcmdflag = "-l -i -c"
 vim.opt.isfname:append("@-@")
 vim.opt.iskeyword:append("_")
 vim.opt.iskeyword:remove("-")
